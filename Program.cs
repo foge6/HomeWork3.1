59adb09900c73de2c83636fb8e4d335e86f4dcf5
makar <makar.t1@mail.ru>
2022-11-30 12:15:46 +0300

    project1

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿// Console.WriteLine("Write first number");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Write second number");
+// int numb1 = Convert.ToInt32(Console.ReadLine());
+// if (numb > numb1)
+//     Console.WriteLine($"number {numb} bigger, tnen {numb1}");
+// else 
+//     Console.WriteLine($"number {numb1} bigger, then {numb}");
+
+
+// Console.WriteLine("Write first number");
+// int numb1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Write second number");
+// int numb2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Write third number");
+// int numb3 = Convert.ToInt32(Console.ReadLine());
+// if 
+//     (numb1>numb2 & numb1>numb3)
+// {
+//     Console.WriteLine($"Number {numb1} the most bigger");
+// }
+// else if  (numb2>numb1 & numb2>numb3)
+// {
+//     Console.WriteLine($"Number {numb2} the most bigger");
+// }
+// else if (numb3>numb2 & numb3>numb1)
+// {
+//     Console.WriteLine($"Number {numb3} the most bigger");
+// }
+
+
+// Console.WriteLine("Write first number");
+// int numb1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Write first number");
+// int numb2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Write first number");
+// int numb3 = Convert.ToInt32(Console.ReadLine());
+// int max = numb1;
+
+// if (max < numb2) 
+
+//     max = numb2;
+
+// if (max < numb3)
+
+//     max = numb3;
+    
+// Console.WriteLine($"Number {max} is bigger");
+
+
+// Console.WriteLine("Write number");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(numb % 2);
+// if (numb % 2 == 1 || numb % 2 == -1)
+// {
+//     Console.WriteLine("Number is odd");
+// }
+// else 
+// {
+//     Console.WriteLine("Number is even");
+// }
+
+// Console.WriteLine("Write number");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// while (count <= numb)
+// {
+// if    (count %2 == 0)
+//     Console.Write(count);
+//     count ++;
+// }
